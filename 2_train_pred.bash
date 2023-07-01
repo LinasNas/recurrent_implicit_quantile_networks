906da6fb4721f331047d74cd13c977c7562c2a9d
LinasNas <lmn2146@columbia.edu>
2023-07-01 17:51:17 +0100

    Update 2_train_pred.bash

--- 2_train_pred.bash
+++ 2_train_pred.bash
@@ -5,4 +5,8 @@
 #SBATCH --gres=gpu:1
 #SBATCH --mail-user=linas.nasvytis@stx.ox.ac.uk
 
+echo “running python3 autoregressive_control.py --test_policy --env_name CartPole-v1 --num_iterations 1000”
+
 python3 autoregressive_control.py --predictive_model_training --env_name CartPole-v1 --is_recurrent_v2 --predictive_model_paths "/vols/teaching/msc-projects/2022-2023/linasvytis/msc/recurrent_implicit_quantile_networks/models/CartPole-v1/replicated_pred_model_1000.pt"
+
+echo “DONE”
