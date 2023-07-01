1ff2b1ee3defa3fd7d07675fc20512f945826dfe
LinasNas <lmn2146@columbia.edu>
2023-07-01 16:53:57 +0100

    Update 2_train_pred.bash

--- 2_train_pred.bash
+++ 2_train_pred.bash
@@ -1,7 +1,8 @@
 #!/bin/bash
+#SBATCH --job 2_train_pred
 #SBATCH --cluster=srf_gpu_01
+#SBATCH --mem 4G
 #SBATCH --gres=gpu:1
-echo “Here are all the GPUs I can see on HPC server ${HOSTNAME}:”
-nvidia-smi -L
+#SBATCH --mail-user=linas.nasvytis@stx.ox.ac.uk
 
-python autoregressive_control.py --test_policy --env_name CartPole-v1 --num_iterations 10000
+python3 autoregressive_control.py --predictive_model_training --env_name CartPole-v1 --is_recurrent_v2 --predictive_model_paths "/vols/teaching/msc-projects/2022-2023/linasvytis/msc/recurrent_implicit_quantile_networks/models/CartPole-v1/replicated_pred_model_1000.pt"
