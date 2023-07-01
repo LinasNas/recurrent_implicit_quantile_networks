a6a35e59e54fb916b1235d169da1428d48dc216f
LinasNas <lmn2146@columbia.edu>
2023-07-01 16:36:03 +0100

    Update 1_create_traject.bash

--- 1_create_traject.bash
+++ 1_create_traject.bash
@@ -1,12 +1,12 @@
 #!/bin/bash
 #SBATCH --job 1_create_trajectories
 #SBATCH --cluster=srf_gpu_01
-#SBATCH --mem 8G
+#SBATCH --mem 4G
 #SBATCH --gres=gpu:1
 #SBATCH --mail-user=linas.nasvytis@stx.ox.ac.uk
 
-echo “running python3 autoregressive_control.py --test_policy --env_name CartPole-v1 --num_iterations 10000”
+echo “running python3 autoregressive_control.py --test_policy --env_name CartPole-v1 --num_iterations 1000”
 
-python3 autoregressive_control.py --test_policy --env_name CartPole-v1 --num_iterations 10000
+python3 autoregressive_control.py --test_policy --env_name CartPole-v1 --num_iterations 1000
 
 echo “DONE”
